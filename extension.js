a344f2394ee7d6a8418888dcdc14c8f36d662340
Englert Bence Brunó <englert.bruno@gmail.com>
2021-12-07 18:15:52 +0100

    update metada.json, update extension.js

--- extension.js
+++ extension.js
@@ -5,11 +5,11 @@ const Me = ExtensionUtils.getCurrentExtension();
 const ScreenBrightnessPanelMenu = Me.imports.ui.ScreenBrightnessPanelMenu;
 
 
+let screenBrightnessPanelMenu;
+
 function init() {
     log(`Initializing ${Me.metadata.name} version ${Me.metadata.version}`);
 }
-
-let screenBrightnessPanelMenu;
 
 function enable() {
     screenBrightnessPanelMenu = new ScreenBrightnessPanelMenu.ScreenBrightnessPanelMenu();
@@ -17,6 +17,8 @@ function enable() {
 }
 
 function disable() {
-    screenBrightnessPanelMenu.destroy();
-    screenBrightnessPanelMenu = null;
+    if (screenBrightnessPanelMenu) {
+        screenBrightnessPanelMenu.destroy();
+        screenBrightnessPanelMenu = null;
+    }
 }
